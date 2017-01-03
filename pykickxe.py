11daa2de18c10b5625ce99517da725faeed4d820
Esgariot <roberutsu@gmail.com>
2017-01-04 00:44:03 +0100

    added dnsmasq config slice, fiddled with main project file, added netinst images/files url list

---
+++ pykickxe.py
@@ -0,0 +1,30 @@
+import urllib
+import argparse
+import url_list.py
+
+
+
+
+# pykickxe
+parse_args()
+# assign to chosen distro
+# assign to tftp-root location
+# unpack() (iso or tar)
+
+
+#modify using subprocess
+
+# ISO_PATH = "your_iso_path_here"
+
+# # Mount the ISO in your OS
+# os.system("mkdir /media/tmp_iso")
+# os.system("mount -o rw,loop %s /media/tmp_iso" % ISO_PATH)
+
+# # Do your Pythonic manipulation here:
+# new_file = open("/media/tmp_iso/.config", 'w')
+# new_file.write(data)
+# new_file.close() 
+
+# # Unmount
+# os.system("umount /media/tmp_iso")
+# os.system("rmdir /media/tmp_iso")
---
+++ url_list.py
@@ -0,0 +1,9 @@
+Date = '03.01.17'
+FedoraURL = (
+    "https://download.fedoraproject.org/pub/fedora/linux/"
+    "releases/25/Workstation/x86_64/iso/"
+    "Fedora-Workstation-netinst-x86_64-25-1.3.iso")
+DebianURL = (
+    "http://ftp.nl.debian.org/debian/dists/jessie/"
+    "main/installer-amd64/current/images/netboot/"
+    "netboot.tar.gz")
