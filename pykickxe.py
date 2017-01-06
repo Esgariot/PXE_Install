ad0054b8b6fce91e4675554c503a8d86050cafd9
Esgariot <roberutsu@gmail.com>
2017-01-06 17:33:15 +0100

    added .gitignore worked on parseArgs

--- pykickxe.py
+++ pykickxe.py
@@ -1,18 +1,88 @@
 import urllib
 import argparse
-import url_list.py
+import pykickxe_conf
+import tempfile
+import os
 
 
+def listConfig():  # TODO: Implement listConfig #p3
+    return
 
 
-# pykickxe
-parse_args()
+def downloadFromURL(args):  # TODO: Implement download #p1
+    distroURL = ''
+    if 'debian' == args.distro:
+        distroURL = pykickxe_conf.debianURL
+    elif 'fedora' == args.distro:
+        distroURL = pykickxe_conf.fedoraURL
+    else:
+        print args.distro + ' is not supported'
+        return
+    if args.verbose is True:
+        print 'downloading ' + args.distro + ' from ' + distroURL
+        os.system('wget ' + distroURL)  # TODO: check wget present #p2
+    # urllib.urlretrieve(distroURL) # TODO: Decide on this or wget #p3
+    # TODO: Check if file already present #p1
+    # TODO: Check filesum #p2
+    return  # TODO: return filename from url #p1
+
+
+def parseArgs():
+    "Parser for cli arguements"
+    parser = argparse.ArgumentParser(
+        prog='Pykickxe',
+        description='A Python PXE and autoinstall tool')
+    parser.add_argument(
+        'distro',
+        type=str,
+        choices=pykickxe_conf.distroList,
+        help='Pick a linux distro to download, unpack and append'
+        'the PXE config to (Debian or Fedora for now)')
+    parser.add_argument(
+        '-l',
+        '--list',
+        help='List some default options',
+        action='store_true')
+    parser.add_argument(
+        '-v',
+        '--verbose',
+        help='Print some of what this is doing at any moment',
+        action='store_true')
+
+    parser.add_argument(
+        '-t',
+        '--tftp_root',
+        type=str,
+        default=pykickxe_conf.tftpRoot,
+        help='Pass an absolute path to tftp root directory')
+    parser.add_argument(
+        '--ks_cfg',
+        type=str,
+        default=pykickxe_conf.ks_cfgPath,
+        help='Pass an absolute path to Kickstart file')
+    parser.add_argument(
+        '--pxe_cfg',
+        type=str,
+        default=pykickxe_conf.pxe_cfgPath,
+        help='Pass an absolute path to pxe file with entry to append')
+    args = parser.parse_args()
+    return args
+
+
+# pykickxe main
+args = parseArgs()  # TODO: Add sensible options and check exclusivity #p3
+if args.list is True:
+    listConfig()
+downloadFromURL(args)
+# TODO: Restructure parser to make sense #p2
+# parse_args() 
+
 # assign to chosen distro
 # assign to tftp-root location
 # unpack() (iso or tar)
 
 
-#modify using subprocess
+# modify using subprocess
 
 # ISO_PATH = "your_iso_path_here"
 
@@ -23,7 +93,7 @@ parse_args()
 # # Do your Pythonic manipulation here:
 # new_file = open("/media/tmp_iso/.config", 'w')
 # new_file.write(data)
-# new_file.close() 
+# new_file.close()
 
 # # Unmount
 # os.system("umount /media/tmp_iso")
--- pykickxe_conf.py
+++ pykickxe_conf.py
@@ -1,9 +1,14 @@
-Date = '03.01.17'
-FedoraURL = (
+# TODO: Write a parser and make this a .conf file or something #p3
+lastCheckedDate = '03.01.17'
+tftpRoot = '/svr/tftp/'
+pxe_cfgPath = './pxeappend.cfg'
+ks_cfgPath = './ks.cfg'
+distroList = ['debian', 'fedora']
+fedoraURL = (
     "https://download.fedoraproject.org/pub/fedora/linux/"
     "releases/25/Workstation/x86_64/iso/"
     "Fedora-Workstation-netinst-x86_64-25-1.3.iso")
-DebianURL = (
+debianURL = (
     "http://ftp.nl.debian.org/debian/dists/jessie/"
     "main/installer-amd64/current/images/netboot/"
     "netboot.tar.gz")
